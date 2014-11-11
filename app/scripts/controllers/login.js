d576ef259d886405033db3c25e98781068874d2e
johna1203 <johnathan@kodokuman.com>
2014-11-11 18:51:07 -0200

    create user and login.

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -8,13 +8,78 @@
  * Controller of the clientApp
  */
 angular.module('loginApp')
-    .controller('LoginCtrl', function ($scope) {
+    .controller('LoginCtrl', function ($scope, $http, Settings) {
 
         $scope.forms = {
             'loginForm': true,
             'forgetForm': false,
             'registerForm': false
-        }
+        };
 
+        $scope.loginFormInput = {
+            username: '',
+            password: ''
+        };
+
+        $scope.registerFormInput = {
+            fullname: '',
+            username: '',
+            password: ''
+        };
+
+        $scope.formErrorMessage = [];
+
+        $scope.formErrorClasses = function () {
+            return {
+                'display-hide': $scope.formErrorMessage.length == 0
+            };
+        };
+
+        $scope.createAnAccount = function () {
+            $scope.forms.loginForm = false;
+            $scope.forms.forgetForm = false;
+            $scope.forms.registerForm = true;
+        };
+
+        $scope.resetFormDisplay = function () {
+            console.log("resetFormDisplay");
+            $scope.forms.loginForm = true;
+            $scope.forms.forgetForm = false;
+            $scope.forms.registerForm = false;
+
+        };
+
+
+        $scope.login = function () {
+            var url = Settings.API_HOST + '/login';
+            $scope.formErrorMessage = [];
+            $http.post(url, $scope.loginFormInput)
+                .success(function (res) {
+                    location.href = 'index.html';
+                }).error(function (res, code) {
+                    console.log('error');
+                    if (code == 401) {
+                        angular.forEach(res.messages, function (msg) {
+                            $scope.formErrorMessage.push(msg);
+                        });
+                    }
+                    console.log(res);
+                });
+        };
+
+        $scope.registerForm = function () {
+            var url = Settings.API_HOST + '/signup';
+            $scope.formErrorMessage = [];
+            $http.post(url, $scope.registerFormInput)
+                .success(function (res) {
+                    location.href = 'index.html';
+                }).error(function (res, code) {
+                    console.log('error');
+                    angular.forEach(res.messages, function (msg) {
+                        $scope.formErrorMessage.push(msg);
+                    });
+                    console.log(res);
+                });
+        };
 
     });
--- app/scripts/services/settings.js
+++ app/scripts/services/settings.js
@@ -27,3 +27,24 @@ angular.module('clientApp')
             }
         };
     });
+
+angular.module('loginApp')
+    .factory('Settings', function () {
+        // Public API here
+        return {
+            IMAGES_CDN: 'https://github.com/coreos/coreos-vagrant/archive/master.zip',
+            LOCAL_IMAGE_ZIP_NAME: 'GeekyOs.zip',
+            LOCAL_VAGRANT_DIR_NAME: 'coreos-vagrant-master',
+            API_HOST: 'http://127.0.0.1:3000',
+            getUserHome: function () {
+                return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+            },
+            getGeekyVar: function () {
+                return this.getUserHome() + '/.geeky';
+            },
+            getGeekyOSVagrantRoot: function () {
+                //return this.getGeekyVar() + '/' + this.LOCAL_VAGRANT_DIR_NAME;
+                return "/home/johna/src/GeekyMenu/GeekyOSVagrant";
+            }
+        };
+    });
