8811ac96749049c6d9b750387ae59197158a4a01
johna1203 <johnathan@kodokuman.com>
2014-12-11 18:20:10 -0200

    organizei a pagina de detalhes de cliente.

--- app/scripts/controllers/customer-detail.js
+++ app/scripts/controllers/customer-detail.js
@@ -8,10 +8,9 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-    .controller('CustomerDetailCtrl', function ($scope) {
-        $scope.awesomeThings = [
-            'HTML5 Boilerplate',
-            'AngularJS',
-            'Karma'
-        ];
+    .controller('CustomerDetailCtrl', function ($scope, $routeParams, Customers) {
+        $scope.enity = null;
+        if ($routeParams.id) {
+            $scope.myPromise = $scope.enity = Customers.get({id: $routeParams.id});
+        }
     });
